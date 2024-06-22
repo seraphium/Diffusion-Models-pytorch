1fc021ab3954ca1adead3d28e3d281d032427185
Jackie Zhang <viperking@163.com>
2024-06-22 17:28:26 +0800

    add support for face datasets, small changes

--- ddpm.py
+++ ddpm.py
@@ -9,11 +9,22 @@ from modules import UNet
 import logging
 from torch.utils.tensorboard import SummaryWriter
 
-logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s: %(message)s",
+    level=logging.INFO,
+    datefmt="%I:%M:%S",
+)
 
 
 class Diffusion:
-    def __init__(self, noise_steps=1000, beta_start=1e-4, beta_end=0.02, img_size=256, device="cuda"):
+    def __init__(
+        self,
+        noise_steps=1000,
+        beta_start=1e-4,
+        beta_end=0.02,
+        img_size=256,
+        device="cuda",
+    ):
         self.noise_steps = noise_steps
         self.beta_start = beta_start
         self.beta_end = beta_end
@@ -21,7 +32,7 @@ class Diffusion:
         self.device = device
 
         self.beta = self.prepare_noise_schedule().to(device)
-        self.alpha = 1. - self.beta
+        self.alpha = 1.0 - self.beta
         self.alpha_hat = torch.cumprod(self.alpha, dim=0)
 
     def prepare_noise_schedule(self):
@@ -29,7 +40,9 @@ class Diffusion:
 
     def noise_images(self, x, t):
         sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None]
-        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None, None, None]
+        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[
+            :, None, None, None
+        ]
         Ɛ = torch.randn_like(x)
         return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * Ɛ, Ɛ
 
@@ -51,7 +64,15 @@ class Diffusion:
                     noise = torch.randn_like(x)
                 else:
                     noise = torch.zeros_like(x)
-                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
+                x = (
+                    1
+                    / torch.sqrt(alpha)
+                    * (
+                        x
+                        - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise
+                    )
+                    + torch.sqrt(beta) * noise
+                )
         model.train()
         x = (x.clamp(-1, 1) + 1) / 2
         x = (x * 255).type(torch.uint8)
@@ -61,8 +82,12 @@ class Diffusion:
 def train(args):
     setup_logging(args.run_name)
     device = args.device
-    dataloader = get_data(args)
+    dataloader = get_face_data(args)
     model = UNet().to(device)
+    print(model)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"total parameters: {total_params}")
+
     optimizer = optim.AdamW(model.parameters(), lr=args.lr)
     mse = nn.MSELoss()
     diffusion = Diffusion(img_size=args.image_size, device=device)
@@ -87,25 +112,30 @@ def train(args):
             logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
 
         sampled_images = diffusion.sample(model, n=images.shape[0])
-        save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))
-        torch.save(model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt"))
+        save_images(
+            sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg")
+        )
+        torch.save(
+            model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt")
+        )
 
 
 def launch():
     import argparse
+
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
     args.run_name = "DDPM_Uncondtional"
     args.epochs = 500
-    args.batch_size = 12
+    args.batch_size = 10
     args.image_size = 64
-    args.dataset_path = r"C:\Users\dome\datasets\landscape_img_folder"
+    args.dataset_path = r"..\crypko_data\faces"
     args.device = "cuda"
     args.lr = 3e-4
     train(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     launch()
     # device = "cuda"
     # model = UNet().to(device)
--- utils.py
+++ utils.py
@@ -4,31 +4,81 @@ import torchvision
 from PIL import Image
 from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from functools import lru_cache
+
+
+class FaceImageDataset(Dataset):
+    def __init__(self, img_dir, transform=None):
+        self.img_dir = img_dir
+        self.transform = transform
+        self.cache = {}
+        self.img_names = os.listdir(img_dir)  # 优化：提前读取文件名列表
+
+    def __len__(self):
+        return len(self.img_names)
+
+    @lru_cache(maxsize=1000)
+    def load_image(self, img_path):
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.img_dir, self.img_names[index])
+        if img_path not in self.cache:
+            self.cache[img_path] = self.load_image(img_path)
+        label = ""
+        return self.cache[img_path], label
 
 
 def plot_images(images):
     plt.figure(figsize=(32, 32))
-    plt.imshow(torch.cat([
-        torch.cat([i for i in images.cpu()], dim=-1),
-    ], dim=-2).permute(1, 2, 0).cpu())
+    plt.imshow(
+        torch.cat(
+            [
+                torch.cat([i for i in images.cpu()], dim=-1),
+            ],
+            dim=-2,
+        )
+        .permute(1, 2, 0)
+        .cpu()
+    )
     plt.show()
 
 
 def save_images(images, path, **kwargs):
     grid = torchvision.utils.make_grid(images, **kwargs)
-    ndarr = grid.permute(1, 2, 0).to('cpu').numpy()
+    ndarr = grid.permute(1, 2, 0).to("cpu").numpy()
     im = Image.fromarray(ndarr)
     im.save(path)
 
 
 def get_data(args):
-    transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(80),  # args.image_size + 1/4 *args.image_size
-        torchvision.transforms.RandomResizedCrop(args.image_size, scale=(0.8, 1.0)),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    transforms = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize(80),  # args.image_size + 1/4 *args.image_size
+            torchvision.transforms.RandomResizedCrop(args.image_size, scale=(0.8, 1.0)),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
     dataset = torchvision.datasets.ImageFolder(args.dataset_path, transform=transforms)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    return dataloader
+
+
+def get_face_data(args):
+    transforms = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize(80),  # args.image_size + 1/4 *args.image_size
+            torchvision.transforms.RandomResizedCrop(args.image_size, scale=(0.8, 1.0)),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+    dataset = FaceImageDataset(args.dataset_path, transforms)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
     return dataloader
 
